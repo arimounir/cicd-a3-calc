785154abdf4141a3f7ca0791425116d1064d74b6
arimounir <ariel.moguillansky827@comunidadunir.net>
2023-05-30 15:12:00 -0300

    Jenkinsfile v1

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -2,23 +2,14 @@ pipeline {
     agent {
         label 'docker'
     }
+    triggers {
+        pollSCM('H/5 * * * *')
+    }
     stages {
         stage('Build') {
             steps {
                 echo 'Building stage!'
                 sh 'make build'
-            }
-        }
-        stage('Unit tests') {
-            steps {
-                sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
-            }
-        }
-        stage('API tests') {
-            steps {
-                sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
         stage('E2E tests') {
