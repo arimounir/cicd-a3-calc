a571c87340fc888e7d69e0460f98c4ef369ee650
arimounir <ariel.moguillansky827@comunidadunir.net>
2023-05-30 15:42:00 -0300

    Jenkinsfile v2

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -12,6 +12,12 @@ pipeline {
                 sh 'make build'
             }
         }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
         stage('E2E tests') {
             steps {
                 sh 'make test-e2e'
