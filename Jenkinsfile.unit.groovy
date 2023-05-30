c8646db01987ac3c8226b9debe74ff1ca8583688
arimounir <ariel.moguillansky827@comunidadunir.net>
2023-05-30 17:36:02 -0300

    agregado source stage2

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -3,9 +3,15 @@ pipeline {
         label 'docker'
     }
     triggers {
+        // Configuración de un trigger que le dice a Jenkins que revise cada 5 minutos la rama asociada para detectar si hubo cambios y disparar el build.
         pollSCM('H/5 * * * *')
     }
     stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/arimounir/cicd-a3-calc.git'
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -14,18 +20,21 @@ pipeline {
         }
         stage('Unit tests') {
             steps {
+                echo 'Unit testing stage!'
                 sh 'make test-unit'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
         stage('API tests') {
             steps {
+                echo 'API testing stage!'
                 sh 'make test-api'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
         stage('E2E tests') {
             steps {
+                echo 'E2E testing stage!'
                 sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
@@ -37,7 +46,8 @@ pipeline {
             cleanWs()
         }
         failure {
-            mail to: 'arielmogui92@gmail.com',
+        // Configuración de envío de mail solo en builds fallidos al correo UNIR del autor. El cuerpo del mail contiene el número de build y nombre de trabajo
+            mail to: 'ariel.moguillansky827@comunidadunir.net',
                 subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
                 body: "Ha ocurrido un fallo. Build #${env.BUILD_NUMBER}. Job name: ${env.JOB_NAME}"
         }
